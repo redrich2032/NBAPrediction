114cf2c19ed9ebddb2f019d92c97452ea7c368d7
redrich2032 <ratcherdnewin2032@gmail.com>
2023-05-19 17:27:57 -0700

    removed random_state for data splitting

--- Models.py
+++ Models.py
@@ -26,9 +26,9 @@ class Models:
         le = LabelEncoder()
         y = le.fit_transform(self.y)
 
+        #split data into training/test set
         from sklearn.model_selection import train_test_split
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X,y, test_size=0.2, random_state=0)
-
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X,y, test_size=0.2)
 
         # feature scaling
         from sklearn.preprocessing import StandardScaler
